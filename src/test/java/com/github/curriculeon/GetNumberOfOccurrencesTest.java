6335fc36e9ee1d8fc3176a4077486ceef80b9667
Leon Hunter <xleonhunter@gmail.com>
2022-08-02 20:06:04 -0400

    updated tests

--- src/test/java/com/github/curriculeon/GetNumberOfOccurrencesTest.java
+++ src/test/java/com/github/curriculeon/GetNumberOfOccurrencesTest.java
@@ -4,31 +4,28 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class GetNumberOfOccurrencesTest {
-
-    private void test(String[] array, int expected) {
-        int actual = StringArrayUtils.getNumberOfOccurrences(array, "bbb");
-        Assert.assertEquals(actual, expected);
-    }
-
     @Test
     public void testGetNumberOfOccurrences1() {
         String[] array = {"aba", "aba", "baa", "bab", "bba", "bba", "bba", "bba", "bbb", "bbb"};
         int expected = 4;
-        test(array, expected);
+        int actual = StringArrayUtils.getNumberOfOccurrences(array, "bba");
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
     public void testGetNumberOfOccurrences2() {
         String[] array = {"aba", "aba", "baa", "bab", "bba", "bba", "bba", "bba", "bbb", "bbb"};
         int expected = 2;
-        test(array, expected);
+        int actual = StringArrayUtils.getNumberOfOccurrences(array, "aba");
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
     public void testGetNumberOfOccurrences3() {
         String[] array = {"aba", "aba", "baa", "bab", "bba", "bba", "bba", "bba", "bbb", "bbb"};
-        int expected = 4;
-        test(array, expected);
+        int expected = 1;
+        int actual = StringArrayUtils.getNumberOfOccurrences(array, "baa");
+        Assert.assertEquals(actual, expected);
     }
 
 }
